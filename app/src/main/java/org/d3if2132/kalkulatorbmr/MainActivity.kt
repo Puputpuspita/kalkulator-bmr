9df4a7620b57acfa9a925ee01438a5d0a2d36873
Puput <puputpuspita@student.telkomuniversity.ac.id>
2022-04-01 17:13:53 +0700

    Menambahkan reset button dan memperbaiki tampilan

--- app/src/main/java/org/d3if2132/kalkulatorbmr/MainActivity.kt
+++ app/src/main/java/org/d3if2132/kalkulatorbmr/MainActivity.kt
@@ -3,6 +3,7 @@ package org.d3if2132.kalkulatorbmr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import org.d3if2132.kalkulatorbmr.databinding.ActivityMainBinding
@@ -18,11 +19,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.button.setOnClickListener { hitungBmr() }
+        binding.resetButton.setOnClickListener { reset() }
 
         val items = listOf("sedikit beraktivitas, tidak berolahraga", "olahraga ringan 1 – 3 kali dalam seminggu", "olahraga ringan 6 - 7 kali dalam seminggu", "olahraga berat 1 atau 2 kali dalam sehari", "olahraga berat 2 kali atau lebih dalam sehari")
         val adapter = ArrayAdapter(this@MainActivity, R.layout.list_item, items)
         val material_spinner = binding.materialSpinner
         material_spinner.setAdapter(adapter)
+
+        binding.card.visibility = View.INVISIBLE
+        binding.card2.visibility = View.INVISIBLE
+        binding.resetButton.visibility = View.INVISIBLE
     }
 
     private fun hitungBmr() {
@@ -121,46 +127,21 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-/*
-        val selectedIdFA = binding.radioGroup2.checkedRadioButtonId
+        binding.resetButton.visibility = View.VISIBLE
+        binding.card.visibility = View.VISIBLE
+        binding.card2.visibility = View.VISIBLE
+    }
 
-        if (selectedIdFA == -1) {
-            Toast.makeText(this, R.string.activity_invalid, Toast.LENGTH_LONG).show()
-            return
-        }
-
-        val isMale = selectedIdJK == R.id.priaRadioButton
-        val aktivitasRingan = selectedIdFA == R.id.aktivitasRingan
-        val aktivitasSedang = selectedIdFA == R.id.aktivitasSedang
-        val aktivitasBerat = selectedIdFA == R.id.aktivitasBerat
-
-        if (isMale) {
-            val bmr = (10 * berat.toFloat()) + (6.5 * tinggi.toFloat()) - (5 * umur.toFloat()) + 5
-            if (aktivitasRingan) {
-                val tdee = 1.6 * bmr
-                binding.hasilBmrTextView.text = getString(R.string.hasil_bmr, tdee)
-            }else if (aktivitasSedang){
-                val tdee = 1.7 * bmr
-                binding.hasilBmrTextView.text = getString(R.string.hasil_bmr, tdee)
-            }else if (aktivitasBerat){
-                val tdee = 1.8 * bmr
-                binding.hasilBmrTextView.text = getString(R.string.hasil_bmr, tdee)
-            }
-        } else {
-            val bmr = (10 * berat.toFloat()) + (6.5 * tinggi.toFloat()) - (5 * umur.toFloat()) - 161
-            if (aktivitasRingan) {
-                val tdee = 1.5 * bmr
-                binding.hasilBmrTextView.text = getString(R.string.hasil_bmr, tdee)
-            }else if (aktivitasSedang){
-                val tdee = 1.6 * bmr
-                binding.hasilBmrTextView.text = getString(R.string.hasil_bmr, tdee)
-            }else if (aktivitasBerat){
-                val tdee = 1.8 * bmr
-                binding.hasilBmrTextView.text = getString(R.string.hasil_bmr, tdee)
-            }
-        }
-
- */
+    private fun reset(){
+        binding.beratBadanInp.getText()?.clear()
+        binding.tinggiBadanInp.getText()?.clear()
+        binding.umurInp.getText()?.clear()
+        binding.radioGroup.clearCheck()
+        binding.hasilBmrTextView.setText("")
+        binding.hasiltdeeTextView.setText("")
+        binding.resetButton.visibility = View.INVISIBLE
+        binding.card.visibility = View.INVISIBLE
+        binding.card2.visibility = View.INVISIBLE
     }
 
 
