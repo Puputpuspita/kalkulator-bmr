93608fd0f8a4c511c141a0fd65a13e1b19d10995
Puput <puputpuspita@student.telkomuniversity.ac.id>
2022-05-24 18:21:26 +0700

    Menambahkan opsi hapus seluruh data histori

--- app/src/main/java/org/d3if2132/kalkulatorbmr/ui/histori/HistoriFragment.kt
+++ app/src/main/java/org/d3if2132/kalkulatorbmr/ui/histori/HistoriFragment.kt
@@ -2,13 +2,13 @@ package org.d3if2132.kalkulatorbmr.ui.histori
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import org.d3if2132.kalkulatorbmr.R
 import org.d3if2132.kalkulatorbmr.databinding.FragmentHistoriBinding
 import org.d3if2132.kalkulatorbmr.db.BmrDb
 
@@ -26,6 +26,7 @@ class HistoriFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentHistoriBinding.inflate(layoutInflater, container, false)
+        setHasOptionsMenu(true)
         return binding.root
     }
 
@@ -43,4 +44,30 @@ class HistoriFragment : Fragment() {
             myAdapter.submitList(it)
         })
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.histori_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_hapus) {
+            hapusSeluruhData()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun hapusSeluruhData() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setMessage(R.string.konfirmasi_hapus)
+            .setPositiveButton(getString(R.string.hapus)) { _, _ ->
+                viewModel.hapusData()
+            }
+            .setNegativeButton(getString(R.string.batal)) { dialog, _ ->
+                dialog.cancel()
+            }
+            .show()
+    }
+
 }
--- app/src/main/java/org/d3if2132/kalkulatorbmr/ui/histori/HistoriViewModel.kt
+++ app/src/main/java/org/d3if2132/kalkulatorbmr/ui/histori/HistoriViewModel.kt
@@ -1,8 +1,19 @@
 package org.d3if2132.kalkulatorbmr.ui.histori
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.d3if2132.kalkulatorbmr.db.BmrDao
 
-class HistoriViewModel(db: BmrDao) : ViewModel() {
+class HistoriViewModel(private val db: BmrDao) : ViewModel() {
     val data = db.getLastBmr()
+
+    fun hapusData() = viewModelScope.launch {
+        withContext(Dispatchers.IO) {
+            db.clearData()
+        }
+    }
+
 }
